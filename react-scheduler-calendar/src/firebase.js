11f0c43f4a327d08b52cf92bba85aadbe74046ef
Michal <michal.kassner@gmail.com>
2024-09-18 21:54:16 +0200

    configuration of react and firebase

---
+++ react-scheduler-calendar/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDy0dKb5d7yxo4O_Et4fmLNzUOjZF6bldk",
+  authDomain: "reactschedulercalendarfb.firebaseapp.com",
+  projectId: "reactschedulercalendarfb",
+  storageBucket: "reactschedulercalendarfb.appspot.com",
+  messagingSenderId: "31502365159",
+  appId: "1:31502365159:web:f1e4e961776552d9838d42"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
